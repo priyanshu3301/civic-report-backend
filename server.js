97f6b4ba010dcd64619f16fabae26ed336f6f70e
priyanshu <priyanshu88011@gmail.com>
2025-10-21 15:19:33 +0530

    feat: initialize basic express server

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
